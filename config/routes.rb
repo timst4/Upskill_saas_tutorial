c22dc0693c7c5df9486e21d76b449afcc9232aa6
Tim McGuigan <huevosrancheros@gmail.com>
2018-03-31 14:38:37 +0000

    Added coding page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  get 'coding', to: 'pages#coding'
 end
